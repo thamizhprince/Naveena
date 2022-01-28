32de2aebf4f5258518b58997dbd9f460d808adb5
thamizhprince <thamizhprince@gmail.com>
2022-01-28 12:36:10 +0530

    Added Files

---
+++ Sample/frmSampleApplication.aspx
@@ -0,0 +1,31 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="frmSampleApplication.aspx.cs" Inherits="Sample.frmSampleApplication" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div> 
+    <table>
+           <tr>
+               <td><asp:Label runat="server" ID="firstname">First Name </asp:Label></td>
+                <td><asp:TextBox runat="server" ID="txtFirstName"></asp:TextBox> </td>
+           </tr>
+         <tr>
+               <td><asp:Label runat="server" ID="lastname">Last Name </asp:Label></td>
+                <td><asp:TextBox runat="server" ID="txtLastName"></asp:TextBox> </td>
+           </tr>
+        <tr>           
+            <td></td>
+            <td><asp:Button ID="btnSave" runat="server"  Text="Save"  BackColor="Green" OnClick="btnSave_Click"/>
+                <asp:Button ID="btnClearAll" runat="server" Text="Clear All" BackColor="RoyalBlue" OnClick="btnClearAll_Click" /> </td>
+        </tr>
+    </table>
+
+    </div>
+    </form>
+</body>
+</html>
---
+++ Sample/frmSampleApplication.aspx.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using Newtonsoft.Json;
+using System.IO;
+using System.Configuration;
+using System.Net;
+
+namespace Sample
+{
+    public partial class frmSampleApplication : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnSave_Click(object sender, EventArgs e)
+        {
+            var vresult = new { FirstName = txtFirstName.Text, LastName = txtLastName.Text };
+            var jsonresult = JsonConvert.SerializeObject(vresult);
+            string filePath = Server.MapPath("~/Result/SampleOutput.json");
+
+            if (!File.Exists(filePath))
+            {
+                File.Create(filePath);
+            }
+            using (StreamWriter sw = File.CreateText(filePath))
+            {
+                sw.WriteLine(Environment.NewLine + "----------- " + DateTime.Today.ToShortDateString() + "--------------");
+                sw.WriteLine(jsonresult);
+            }
+            var fi = new FileInfo(filePath);
+            Response.Clear();
+            Response.ContentType = "application/octet-stream";
+            Response.AddHeader("Content-Disposition", "attachment; filename=" + fi.Name);
+            Response.WriteFile(filePath);
+            Response.End();
+
+            txtFirstName.Text = string.Empty;
+            txtLastName.Text = string.Empty;
+           
+        }
+        protected void btnClearAll_Click(object sender, EventArgs e)
+        {
+            txtFirstName.Text = string.Empty;
+            txtLastName.Text = string.Empty;
+
+        }
+    }
+}
